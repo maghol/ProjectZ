251bb6a1ba2774a5afe30126ef929cf2df8299ec
Mathilda <chattaren_@hotmail.com>
2018-02-05 15:20:16 +0100

    Added race icons on the randomize race activity

--- app/src/main/java/com/zed/projectz/RaceHelper.java
+++ app/src/main/java/com/zed/projectz/RaceHelper.java
@@ -7,23 +7,23 @@ import java.util.Random;
 
 public class RaceHelper {
     private List<Race> Races = new ArrayList<>(Arrays.asList(
-            new Race("Sardakk N'Orr", 0),
-            new Race("The Arborec", 1),
-            new Race("The Barony of Letnev", 2),
-            new Race("The Clan of Saar", 3),
-            new Race("The Embers of Muaat", 4),
-            new Race("The Emirates of Hacan", 5),
-            new Race("The Federation of Sol", 6),
-            new Race("The Ghosts of Creuss", 7),
-            new Race("The L1z1x Mindnet", 8),
-            new Race("The Mentak Coalition", 9),
-            new Race("The Naalu Collective", 10),
-            new Race("The Nekro Virus", 11),
-            new Race("The Universities of Jol-Nar", 12),
-            new Race("The Winnu", 13),
-            new Race("The Xxcha Kingdom", 14),
-            new Race("The Yin Brotherhood", 15),
-            new Race("The Yssaril Tribes", 16)
+            new Race("Sardakk N'Orr", 0, 0),
+            new Race("The Arborec", 1, 1),
+            new Race("The Barony of Letnev", 2, 2),
+            new Race("The Clan of Saar", 3, 3),
+            new Race("The Embers of Muaat", 4, 4),
+            new Race("The Emirates of Hacan", 5, 5),
+            new Race("The Federation of Sol", 6, 6),
+            new Race("The Ghosts of Creuss", 7, 7),
+            new Race("The L1z1x Mindnet", 8, 8),
+            new Race("The Mentak Coalition", 9, 9),
+            new Race("The Naalu Collective", 10, 10),
+            new Race("The Nekro Virus", 11, 11),
+            new Race("The Universities of Jol-Nar", 12, 12),
+            new Race("The Winnu", 13, 13),
+            new Race("The Xxcha Kingdom", 14, 14),
+            new Race("The Yin Brotherhood", 15, 15),
+            new Race("The Yssaril Tribes", 16, 16)
     ));
 
     public List<Player> RandomizeRaces(List<Player> players, int numberOfRacesPerPlayer) {
